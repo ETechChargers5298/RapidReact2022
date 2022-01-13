be5e2872eecfc72bb78e993f1bae572f2c12c2da
botkii <kennw902@gmail.com>
2022-01-12 21:28:59 -0500

    Added comments to ArcadeDrive.java

--- src/main/java/frc/robot/commands/ArcadeDrive.java
+++ src/main/java/frc/robot/commands/ArcadeDrive.java
@@ -10,18 +10,23 @@ import frc.robot.subsystems.Drivetrain;
 
 public class ArcadeDrive extends CommandBase {
   
+  // Drivetrain subsystem needed for this command
   private Drivetrain drivetrain;
   
+  // Supplies the speeds from the controller
   private DoubleSupplier linear;
   private DoubleSupplier rotational;
 
-  /** Creates a new ArcadeDRive. */
+  /** Creates a new ArcadeDrive. */
   public ArcadeDrive(Drivetrain drivetrain, DoubleSupplier linear, DoubleSupplier rotational) {
     
+    // Obtains drivetrain from RobotContainer
     this.drivetrain = drivetrain;
 
+    // Obtains controller inputs from RobotContainer
     this.linear = linear;
     this.rotational = rotational; 
+    
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(drivetrain);
   }
@@ -29,24 +34,31 @@ public class ArcadeDrive extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    
+    // Robot starts out not moving
     drivetrain.motorStop();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    
+    // Robot will move depending on left joystick 
     drivetrain.arcadeDrive(linear.getAsDouble(), rotational.getAsDouble());
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    // Robot will stop when we stop running code
     drivetrain.motorStop();
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
+    
+    // Driving shall never stop.
     return false;
   }
 }
