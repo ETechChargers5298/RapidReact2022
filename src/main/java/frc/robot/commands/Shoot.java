95f09923ae5c3293947af6d2ab27ad9582fe8a1a
BotRamenRobotics <redstonegamer360@gmail.com>
2022-02-03 19:51:55 -0500

    Created Shooter subsystem and Shoot command

---
+++ src/main/java/frc/robot/commands/Shoot.java
@@ -0,0 +1,43 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Shooter;
+
+public class Shoot extends CommandBase {
+
+  private Shooter pew; 
+
+  /** Creates a new Shoot. */
+  public Shoot(Shooter shoot) {
+    pew = shoot;
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+
+    // if Velocity is over 1000 it will shoot.
+   if (pew.getFlyVelocity() > 1000) {
+     pew.feed();
+   }
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
